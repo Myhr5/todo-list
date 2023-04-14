10f14d8a213e287e4103291508660a834f7ca461
Mirele Fernandes <mirele.mrf@gmail.com>
2023-04-14 13:48:47 -0300

    final commit

--- tasks.js
+++ tasks.js
@@ -5,12 +5,7 @@ function uid() {
 let taskData = [
   {
     id: uid(),
-    name: "Vê se eu tô na esquina",
-    toDo: true,
-  },
-  {
-    id: uid(),
-    name: "Dar banho nos gatos",
+    name: "Crie uma nova tarefa",
     toDo: false,
   },
 ];
@@ -18,6 +13,37 @@ let taskData = [
 const addTaskInput = document.getElementById("task_input");
 const addTaskButton = document.getElementsByTagName("button")[0];
 const tasklist = document.getElementById("tasks_list");
+const todoCounterText = document.getElementById("todo_count");
+const doneCounterText = document.getElementById("done_count");
+const emptyTasks = document.getElementById("empty_tasks");
+
+// empty tasks
+function verifyIfListIsEmpty() {
+  if (taskData.length === 0) {
+    emptyTasks.classList.remove("hidden");
+  } else {
+    emptyTasks.classList.add("hidden");
+  }
+}
+
+function counter() {
+  let toDoCounter = 0;
+  let doneCounter = 0;
+
+  toDoCounter = taskData.length;
+  todoCounterText.innerHTML = `${toDoCounter}`;
+
+  for (const task of taskData) {
+    if (task.toDo === false) {
+      doneCounter++;
+    }
+  }
+
+  doneCounterText.innerText = `${doneCounter}`;
+}
+
+verifyIfListIsEmpty();
+counter();
 
 //create new task element
 function createNewTaskEl(taskName, taskId) {
@@ -82,23 +108,88 @@ function addTask(event) {
   taskData.push(newTask);
   const taskElement = createNewTaskEl(newTask.name, newTask.id);
   tasklist.appendChild(taskElement);
+
+  addTaskInput.value = "";
+  counter();
+  verifyIfListIsEmpty();
 }
 
 // complete task
 function completeTask(event) {
   console.log("Complete Task");
+
+  const todoIcon = event.target;
+  todoIcon.classList.add("hidden");
+
+  const text = todoIcon.parentNode.childNodes[2];
+  text.classList.add("risked");
+
+  const taskToCompletId = todoIcon.parentNode.parentNode.id;
+  const taskToComplete = document.getElementById(taskToCompletId);
+
+  taskToComplete.classList.add("done");
+  taskToComplete.classList.remove("todo");
+
+  const doneIcon = todoIcon.parentNode.childNodes[1];
+  doneIcon.classList.remove("hidden");
+
+  taskData.find((item) => {
+    if (item.id === taskToCompletId) {
+      item.toDo = false;
+    }
+  });
+
+  counter();
 }
 
 // incomplete task
 function incompleteTask(event) {
   console.log("Incomplete Task");
+
+  const doneIcon = event.target;
+  doneIcon.classList.add("hidden");
+
+  const text = doneIcon.parentNode.childNodes[2];
+  text.classList.remove("risked");
+
+  const taskToIncompleteId = doneIcon.parentNode.parentNode.id;
+  const taskToIncomplete = document.getElementById(taskToIncompleteId);
+
+  taskToIncomplete.classList.add("todo");
+  taskToIncomplete.classList.remove("done");
+
+  const todoIcon = doneIcon.parentNode.childNodes[0];
+  todoIcon.classList.remove("hidden");
+
+  taskData.find((item) => {
+    if (item.id === taskToIncompleteId) {
+      item.toDo = true;
+    }
+  });
+
+  counter();
 }
 
 // delete task
 function deleteTask(event) {
   console.log("Delete Task");
+
+  const taskToDeleteId = event.target.parentNode.id;
+  const taskToDelete = document.getElementById(taskToDeleteId);
+
+  const tasksWithoutDeletedOne = taskData.filter((task) => {
+    return task.id !== taskToDeleteId;
+  });
+
+  taskData = tasksWithoutDeletedOne;
+  tasklist.removeChild(taskToDelete);
+
+  counter();
+  verifyIfListIsEmpty();
 }
 
 // sync HTML with taskData list
-
-// counter tasks
+for (const task of taskData) {
+  const taskItem = createNewTaskEl(task.name, task.id);
+  tasklist.appendChild(taskItem);
+}
