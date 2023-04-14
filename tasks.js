c8d8d44dc1b53744158d46a8b04ceeb979f8266b
Mirele Fernandes <mirele.mrf@gmail.com>
2023-04-13 21:58:43 -0300

    JS criar nova task

--- tasks.js
+++ tasks.js
@@ -1,11 +1,15 @@
+function uid() {
+  return Date.now().toString(16) + Math.random().toString(16).substring(2);
+}
+
 let taskData = [
   {
-    id: 1,
+    id: uid(),
     name: "Vê se eu tô na esquina",
     toDo: true,
   },
   {
-    id: 2,
+    id: uid(),
     name: "Dar banho nos gatos",
     toDo: false,
   },
@@ -15,10 +19,68 @@ const addTaskInput = document.getElementById("task_input");
 const addTaskButton = document.getElementsByTagName("button")[0];
 const tasklist = document.getElementById("tasks_list");
 
+//create new task element
+function createNewTaskEl(taskName, taskId) {
+  // create task li
+  let task = document.createElement("li");
+  task.classList.add("task");
+  task.classList.add("todo");
+  task.setAttribute("id", taskId);
+  // crate .left_content div
+  let leftContent = document.createElement("div");
+  leftContent.classList.add("left_content");
+
+  //todo icon
+  let todoIcon = document.createElement("i");
+  todoIcon.classList.add("ph-duotone");
+  todoIcon.classList.add("ph-circle");
+  todoIcon.classList.add("check_btn");
+  todoIcon.addEventListener("click", completeTask);
+
+  //done icon
+  let doneIcon = document.createElement("i");
+  doneIcon.classList.add("ph-duotone");
+  doneIcon.classList.add("ph-check-circle");
+  doneIcon.classList.add("check_btn");
+  doneIcon.addEventListener("click", incompleteTask);
+
+  //task name /p
+  let name = document.createElement("p");
+  name.innerHTML = taskName;
+
+  //delete icon
+  let deleteIcon = document.createElement("i");
+  deleteIcon.classList.add("ph-duotone");
+  deleteIcon.classList.add("ph-trash");
+  deleteIcon.classList.add("delete_btn");
+  deleteIcon.addEventListener("click", deleteTask);
+
+  leftContent.appendChild(todoIcon);
+  leftContent.appendChild(doneIcon);
+  leftContent.appendChild(name);
+
+  leftContent.appendChild(leftContent);
+  leftContent.appendChild(deleteIcon);
+
+  return task;
+}
+
 // add new task
 function addTask(event) {
   event.preventDefault();
   console.log("Add Task");
+
+  const newTaskName = addTaskInput.value;
+
+  const newTask = {
+    id: uid(),
+    name: newTaskName,
+    toDo: true,
+  };
+
+  taskData.push(newTask);
+  const taskElement = createNewTaskEl(newTask.name, newTask.id);
+  tasklist.appendChild(taskElement);
 }
 
 // complete task
